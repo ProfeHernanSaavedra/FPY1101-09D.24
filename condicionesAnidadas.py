e818a0d8b13106a197a17929c58cfd4720e59baa
USRVI-LC4 <USRVI-LC4@duoc.acad>
2024-04-17 16:56:47 -0400

    agregue cambio

---
+++ condicionesAnidadas.py
@@ -0,0 +1,22 @@
+##condiciones anidadas
+
+añoActual = int(input("Ingrese año Actual: "))
+añoNac = int(input("Ingrese año de nacimiento: "))
+
+edad = añoActual - añoNac
+
+print("Su edad es : ",edad," años app")
+
+if edad >= 18 :
+    print("UD es mayor de edad")
+else:
+    if edad > 0 and edad <= 4:
+        print("Ud es un bebe")
+    else:
+        if edad >4 and edad <=12:
+            print("es un niño")
+        else:
+            if edad >12 and edad <18:
+                print("ud es una adolescente") 
+                
+            
